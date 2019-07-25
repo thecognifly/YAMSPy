870ce134e7e48bcde899a05e8d213e1312ef0130
Ricardo de Azambuja <ricardo.azambuja@gmail.com>
2019-07-25 15:31:09 -0400

    Standard comments added

--- Examples/fc_reboot.py
+++ Examples/fc_reboot.py
@@ -1,3 +1,30 @@
+"""fc_reboot.py: Reboot your flight controller (Betaflight)
+
+Copyright (C) 2019 Ricardo de Azambuja
+
+This file is part of BetaflightMSPy.
+
+BetaflightMSPy is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+BetaflightMSPy is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BetaflightMSPy.  If not, see <https://www.gnu.org/licenses/>.
+
+Acknowledgement:
+This work was possible thanks to the financial support from IVADO.ca (postdoctoral scholarship 2019/2020).
+
+Disclaimer (adapted from Wikipedia):
+None of the authors, contributors, supervisors, administrators, employers, friends, family, vandals, or anyone else 
+connected (or not) with this project, in any way whatsoever, can be made responsible for your use of the information (code) 
+contained or linked from here.
+"""
 import time
 from betaflightmspy import MSPy
 
