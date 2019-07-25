8b15c1d8b37d43a99e886f65b89ed5060739f5d5
cmftom <38613993+cmftom@users.noreply.github.com>
2019-07-25 18:54:57 -0400

    Start using Muti-Processing

--- PiZero_Control.py
+++ PiZero_Control.py
@@ -2,7 +2,9 @@ import os
 import io
 import subprocess
 import signal
-from optical_flow import Filter
+from optical_flow import Filter, Video
+from multiprocessing import Pool, Process
+import time
 
 os.nice(-10) # Main loop is highest proity 
 video_process = None
@@ -15,12 +17,19 @@ try:
                         diff_threshold = 0.3,
                         data_threshold = 5, 
                         altitude = 100)
+    video = Video(frameWidth=240, frameHeight=240, frameRate=10)
     # frameWidth, frameHeight, frameRate
-    video_process = subprocess.Popen(["python3", 'optical_flow.py', "VIDEOSTREAM", str(240), str(240), str(10)], preexec_fn = nice)
+    # video_process = subprocess.Popen(["python3", 'optical_flow.py', "VIDEOSTREAM", str(240), str(240), str(10)], preexec_fn = nice)
+    video_process = Process(target = video.run, args=())
+    print("start camera")
+    video_process.start()
     video_fifo = io.open("/dev/shm/motion_stream", mode="rb")
+    print("OK")
+    
     while True:
         try:
             # Input the data into filter
+            time.sleep(.01)
             dz, dx, dy, gnd_x, gnd_y = filter.run(video_fifo.read(filter.data_size))
             print(dx, dy)
         except KeyboardInterrupt:
@@ -32,9 +41,10 @@ except FileExistsError:
 finally:  
     # .poll() return None when it is not terminated
     if video_process:
-        if video_process.poll() == None:
+        if video_process.is_alive():
             # video_process.send_signal(signal.SIGINT) #Parent kill, all kill
-            video_process.wait()
+            video_process.terminate()
+            video_process.join()
         video_fifo.close()
     os.unlink("/dev/shm/motion_stream")
     print ("\nFINISH!!!!!!")
