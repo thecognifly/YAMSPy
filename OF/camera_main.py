064e88fb1f8bd4fb9d0ec94f71d64908cc9946c7
cmftom <38613993+cmftom@users.noreply.github.com>
2019-07-30 13:55:53 -0400

    Update Debug and Pipe

--- OF/camera_main.py
+++ OF/camera_main.py
@@ -1,32 +1,45 @@
 from camera import Camera
-
 from multiprocessing import Pool, Process, Pipe
 import time
 
 camera = Camera(frameWidth=240,
                     frameHeight=240,
-                    frameRate = 90)
+                    frameRate = 30,
+                    DEBUG=True)
 camera_start = Process(target=camera.run,
                             args=())
+
+def motion_data():
+    ctn = 0
+    data_ = camera.read_motion()
+    data = camera.read_motion()
+    if data_ is not None:
+        while data is None and ctn < 5:
+            data = camera.read_motion()
+            ctn += 1
+        if data is None:
+            data = data_
+    return data
+
+def cv_data():
+    ctn = 0
+    data_ = camera.read_cv()
+    data = camera.read_cv()
+    if data_ is not None:
+        while data is None and ctn < 5:
+            data = camera.read_cv()
+            ctn += 1
+        if data is None:
+            data = data_
+    return data
 
 try:
     camera_start.start()
     while True:
         start = time.time()
-        ctn = 0
         time.sleep(.1)
-        data_ = camera.read_motion()
-        data = camera.read_motion()
-        if data_ is not None:
-            while data is None and ctn < 5:
-                data = camera.read_motion()
-                ctn += 1
-            if data is None:
-                data = data_
-            # print (data)
-        # print ("b-",data['x'][10])
-        else:
-            continue
+        motion_data()
+        cv_data()
         # print("MAIN - Running at {}Hz".format(1/(time.time()-start)))
         
 
