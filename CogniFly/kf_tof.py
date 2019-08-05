6c8c1247131ab7a6b70787a3585e72a63e120fdf
cmftom <38613993+cmftom@users.noreply.github.com>
2019-08-05 16:48:17 -0400

    Update kf_tof.py

--- CogniFly/kf_tof.py
+++ CogniFly/kf_tof.py
@@ -1,7 +1,9 @@
-# pip install filterpy
+# !pip install filterpy
 # https://filterpy.readthedocs.io/en/latest/
-import filterpy
+import time
 import numpy as np
+
+import filterpy
 from filterpy.kalman import KalmanFilter
 from filterpy.common import Q_discrete_white_noise
 
@@ -9,44 +11,41 @@ from filterpy.common import Q_discrete_white_noise
 # dim_z is number of measurement
 # The output will become [displacement
 #                           velocity  ]
-tof_filter = KalmanFilter(dim_x=2, dim_z=1)
+tof_filter = KalmanFilter(dim_x=2, dim_z=1, dim_u = 1)
 
-# t is the time different between each collected data
-t = None
-
-
-# initial value from the sensor
-# Assume the 
-tof_filter.x = None 
-
-# The Sensor Model
-tof_filter.F = np.array([[1, t],
-                         [0, 1]]) 
-# Control Matrix
-# in cm/s
-tof_filter.B = np.array([[0.5*(t**2)*(-98.1)],
-                         [t*(-98.1)]]) 
-
-# Measurement Matrix
-tof_filter.H = np.array([[1, 0]])
-
-# covariance matrix
-tof_filter.P *= .1     
-
-# Process covariance
-tof_filter.Q *= 0.005 
-
-# Measurement covariance
-tof_filter.R = np.array([[0.7]])
-
-def tof():
-    tof_filter.predict()
-    return (tof_filter.x)
-
-def update(data, _t):
-    global t
-    if not tof_filter.x:
+def init(freq ,data):
+        # Sleep Time:
+        global t
+        t = 1/freq
+        # initial value from the sensor
         tof_filter.x = np.array([[data], 
                                 [0]]) 
-    t = _t
-    tof_filter.update([data])
+        # The Sensor Model
+        tof_filter.F = np.array([[1, t],
+                                [0, 1]]) 
+        # Control Matrix
+        tof_filter.B = np.array([[0.5*(t**2)*(1)],
+                                [t*(1)]]) 
+
+        # Measurement Matrix
+        tof_filter.H = np.array([[1, 0]])
+
+        # covariance matrix
+        tof_filter.P *= .1 
+
+        # Process covariance
+        tof_filter.Q *= 0
+
+        # Measurement covariance
+        tof_filter.R = np.array([[1]])
+
+def tof(pipe_read, pipe_write):
+        global t
+        while True:
+                tof_filter.predict()
+                if not pipe_read.poll():
+                        pipe_write.send(tof_filter.x)
+                time.sleep(t)
+
+def update(data):
+        tof_filter.update([data])
