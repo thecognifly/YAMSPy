6a8d8c0cbd753d03158f924181b2c989acc7240e
cmftom <38613993+cmftom@users.noreply.github.com>
2019-08-06 11:24:09 -0400

    Update kf_tof.py

--- CogniFly/kf_tof.py
+++ CogniFly/kf_tof.py
@@ -27,7 +27,7 @@ tof_process = Process(target = tof.run, args = (5,))
 tof_process.start()
 
 # Sleep Time:
-t = 1/TofFilter_freq
+dt = 1/TofFilter_freq
 # Set up the filter
 tof_filter = KalmanFilter(dim_x = 2, dim_z = 1, dim_u = 1)
 
@@ -36,37 +36,36 @@ tof_filter = KalmanFilter(dim_x = 2, dim_z = 1, dim_u = 1)
 tof_filter.x = np.array([[0.11], 
                         [0]]) 
 # The Sensor Model
-tof_filter.F = np.array([[1, t],
+tof_filter.F = np.array([[1, dt],
                         [0, 1]]) 
 # Control Matrix
-tof_filter.B = np.array([[0.5*(t**2)*(1)],
-                        [t*(1)]]) 
+tof_filter.B = np.array([[0.5*(dt**2)*(1)],
+                        [dt*(1)]]) 
 
 # Measurement Matrix
 tof_filter.H = np.array([[1, 0]])
 
 # covariance matrix
-tof_filter.P *= 0.01 
+tof_filter.P *= np.array([[0.01, 0],
+                          [0 ,   0]])
 
 # Process covariance
 tof_filter.Q *= 0
 
 # Measurement covariance
-# Noise of he sensor ~0.05m
-tof_filter.R = np.array([[0.05]])
+# Noise of he sensor ~0.005m (5mm)
+tof_filter.R = np.array([[0.005]])
 
 try:
         while True:
                 try:
+                        # u is the acceration of the z-axis
+                        tof_filter.predict(u = [0])
                         # >>> Read data
                         if control_ToF_pipe_read.poll():
                                 tof_filter.update([control_ToF_pipe_read.recv()])
-                                print ("Update")
-                        else:
-                                # u is the acceration of the z-axis
-                                tof_filter.predict(u = (0))
                         print (tof_filter.x)
-                        time.sleep(t)
+                        time.sleep(dt)
                 except KeyboardInterrupt:
                         break
 
