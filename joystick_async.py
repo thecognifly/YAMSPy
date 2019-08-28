df497039843fec299986eee6e7600a676ab1886c
cmftom <cmftom@gmail.com>
2019-08-28 14:28:27 -0400

    add all auto

--- joystick_async.py
+++ joystick_async.py
@@ -72,7 +72,7 @@ DATA = []
 
 
 # List of inputs that will be taken over when in autonomous mode
-AUTONOMOUS_INPUT = [] #['roll', 'pitch', 'throttle']
+AUTONOMOUS_INPUT = ['roll', 'pitch','throttle'] #['roll', 'pitch', 'throttle']
 
 # Using MSP controller it's possible to have more auxiliary inputs than this.
 CMDS_init = {
