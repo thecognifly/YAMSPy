0eda0abb673dcf949f3cf7da880e18b7cd817776
ricardodeazambuja <ricardo.azambuja@gmail.com>
2020-10-21 19:09:50 +0100

    Now the version number comes from __init__.py

--- setup.py
+++ setup.py
@@ -1,7 +1,25 @@
 from setuptools import setup, find_packages
+import codecs
+import os.path
+
+
+# from https://packaging.python.org/guides/single-sourcing-package-version/
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 setup(
     name="YAMSPy",
-    version="0.2",
+    version=get_version("yamspy/__init__.py"),
     packages=['yamspy'],
     install_requires=['pyserial'],
 
