cc4759f50e2dd2d9df148206bbadaf6921c6068b
ricardodeazambuja <ricardo.azambuja@gmail.com>
2022-10-30 16:08:43 -0400

    runs from a list so oit's easier to add other messages

--- Examples/test_msp2.py
+++ Examples/test_msp2.py
@@ -15,29 +15,19 @@ from yamspy import MSPy
 #
 serial_port = "/dev/ttyACM0"
 
+msg_list = ['MSP2_INAV_STATUS', 'MSP2_INAV_ANALOG']
 with MSPy(device=serial_port, loglevel='DEBUG', baudrate=115200) as board:
     while True:
-        prev = time.monotonic()
-        # Read info from the FC
-        # Please, pay attention to the way it works:
-        # 1. Message is sent without any payload (data=[])
-        if board.send_RAW_msg(MSPy.MSPCodes['MSP2_INAV_STATUS'], data=[]):
-            # 2. Response msg from the flight controller is received
-            dataHandler = board.receive_msg()
-            # 3. The msg is parsed
-            # board.process_recv_data(dataHandler)
-            # 4. After the parser, the instance is populated.
-            # In this example, SENSOR_DATA has its altitude value updated.
-            print(dataHandler)
-        print(f"MSP2_INAV_STATUS: f={1/(time.monotonic()-prev):0.3}Hz")
-
-        prev = time.monotonic()
-        if board.send_RAW_msg(MSPy.MSPCodes['MSP2_INAV_ANALOG'], data=[]):
-            # 2. Response msg from the flight controller is received
-            dataHandler = board.receive_msg()
-            # 3. The msg is parsed
-            # board.process_recv_data(dataHandler)
-            # 4. After the parser, the instance is populated.
-            # In this example, SENSOR_DATA has its altitude value updated.
-            print(dataHandler)
-        print(f"MSP2_INAV_ANALOG: f={1/(time.monotonic()-prev):0.3}Hz")
+        for msg in msg_list:
+            prev = time.monotonic()
+            # Read info from the FC
+            # Please, pay attention to the way it works:
+            # 1. Message is sent without any payload (data=[])
+            if board.send_RAW_msg(MSPy.MSPCodes[msg], data=[]):
+                # 2. Response msg from the flight controller is received
+                dataHandler = board.receive_msg()
+                # 3. The msg is parsed
+                # board.process_recv_data(dataHandler)
+                # 4. After the parser, the instance is populated.
+                print(dataHandler)
+            print(f"{msg}: f={1/(time.monotonic()-prev):0.3}Hz")
