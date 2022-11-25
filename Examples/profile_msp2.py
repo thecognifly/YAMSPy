c0d8822ba26f197d7fdb25cdf14f5be7b7053a40
ricardodeazambuja <ricardo.azambuja@gmail.com>
2022-11-25 10:33:49 -0500

    Small additions to examples

--- Examples/profile_msp2.py
+++ Examples/profile_msp2.py
@@ -1,4 +1,11 @@
+from math import cos
+
+from math import cos
 import time
+import datetime
+import struct
+import os
+
 import cProfile
 import pstats
 from pstats import SortKey
@@ -20,10 +27,76 @@ serial_port = "/dev/ttyACM0"
 
 profile = cProfile.Profile()
 
+
+DISTANCE_BETWEEN_TWO_LONGITUDE_POINTS_AT_EQUATOR = 1.113195
+long_origin = -73.61319383049725 * 10000000
+lat_origin = 45.50496682273918 * 10000000
+gpsScaleLonDown = cos((abs(lat_origin) / 10000000) * 0.0174532925)
+
+lat_displace = lambda x: x/DISTANCE_BETWEEN_TWO_LONGITUDE_POINTS_AT_EQUATOR + lat_origin
+long_displace = lambda y: y/(gpsScaleLonDown*DISTANCE_BETWEEN_TWO_LONGITUDE_POINTS_AT_EQUATOR) + long_origin
+
+msp2_gps_format = '<BHIBBHHHHiiiiiiHHHBBBBB' # https://docs.python.org/3/library/struct.html#format-characters
+gps_template = {
+             'instance': 0,                  # uint8 -  sensor instance number to support multi-sensor setups
+             'gpsWeek':  0,                  # uint16 - GPS week, 0xFFFF if not available
+             'msTOW': 0,                     # uint32
+             'fixType': 0,                   # uint8
+             'satellitesInView': 0,          # uint8
+             'horizontalPosAccuracy': 0,      # uint16 - [cm]
+             'verticalPosAccuracy': 0,        # uint16 - [cm]
+             'horizontalVelAccuracy': 0,      # uint16 - [cm/s]
+             'hdop': 0,                       # uint16
+             'longitude': 0,                  # int32
+             'latitude': 0,                   # int32
+             'mslAltitude': 0,                # int32 - [cm]
+             'nedVelNorth': 0,                # int32 - [cm/s]
+             'nedVelEast': 0,                 # int32
+             'nedVelDown': 0,                 # int32
+             'groundCourse': 0,               # uint16 - deg * 100, 0..36000
+             'trueYaw': 0,                    # uint16 - deg * 100, values of 0..36000 are valid. 65535 = no data available
+             'year': 0,                       # uint16
+             'month': 0,                      # uint8
+             'day': 0,                        # uint8
+             'hour': 0,                       # uint8
+             'min': 0,                        # uint8
+             'sec': 0,                        # uint8
+}
+
+mspSensorGpsDataMessage = gps_template.copy()
+mspSensorGpsDataMessage['instance'] = 1
+mspSensorGpsDataMessage['fixType'] = 99
+mspSensorGpsDataMessage['satellitesInView'] = mspSensorGpsDataMessage['fixType']
+mspSensorGpsDataMessage['gpsWeek'] = 0xFFFF
+
+############ SEND GPS DATA #############
+# gpsSol.llh.lon   = pkt->longitude;
+mspSensorGpsDataMessage['longitude'] = long_origin
+# gpsSol.llh.lat   = pkt->latitude;
+mspSensorGpsDataMessage['latitude'] = lat_origin
+# gpsSol.llh.alt   = pkt->mslAltitude;
+mspSensorGpsDataMessage['mslAltitude'] = 0 # [cm]
+# gpsSol.velNED[X] = pkt->nedVelNorth;
+mspSensorGpsDataMessage['nedVelNorth'] = 0
+# gpsSol.velNED[Y] = pkt->nedVelEast;
+mspSensorGpsDataMessage['nedVelEast'] = 0
+# gpsSol.velNED[Z] = pkt->nedVelDown;
+mspSensorGpsDataMessage['nedVelDown'] = 0
+# gpsSol.groundSpeed = calc_length_pythagorean_2D((float)pkt->nedVelNorth, (float)pkt->nedVelEast);
+# gpsSol.groundCourse = pkt->groundCourse / 10;   // in deg * 10
+mspSensorGpsDataMessage['groundCourse'] = 15000 # deg * 100, 0..36000
+mspSensorGpsDataMessage['trueYaw'] = 15000
+# gpsSol.eph = gpsConstrainEPE(pkt->horizontalPosAccuracy / 10);
+mspSensorGpsDataMessage['horizontalPosAccuracy'] = 10
+# gpsSol.epv = gpsConstrainEPE(pkt->verticalPosAccuracy / 10);
+mspSensorGpsDataMessage['verticalPosAccuracy'] = 10
+# gpsSol.hdop = gpsConstrainHDOP(pkt->hdop);
+mspSensorGpsDataMessage['hdop'] = 10
+
 def foo():
-    with MSPy(device=serial_port, loglevel='DEBUG', baudrate=1152000) as board:
+    with MSPy(device=serial_port, loglevel='DEBUG', baudrate=115200) as board:
+        prev = time.monotonic()
         for i in range(100):
-            prev = time.monotonic()
             # Read info from the FC
             # Please, pay attention to the way it works:
             # 1. Message is sent without any payload (data=[])
@@ -35,7 +108,6 @@ def foo():
                 # 4. After the parser, the instance is populated.
                 # In this example, SENSOR_DATA has its altitude value updated.
 
-            prev = time.monotonic()
             if board.send_RAW_msg(MSPy.MSPCodes['MSP2_INAV_ANALOG'], data=[]):
                 # 2. Response msg from the flight controller is received
                 dataHandler = board.receive_msg()
@@ -44,6 +116,18 @@ def foo():
                 # 4. After the parser, the instance is populated.
                 # In this example, SENSOR_DATA has its altitude value updated.
 
+            now = datetime.datetime.now()
+            mspSensorGpsDataMessage['year'] = now.year
+            mspSensorGpsDataMessage['month'] = now.month
+            mspSensorGpsDataMessage['day'] = now.day
+            mspSensorGpsDataMessage['hour'] = now.hour
+            mspSensorGpsDataMessage['min'] = now.minute
+            mspSensorGpsDataMessage['sec'] = now.second
+            gps_data = struct.pack(msp2_gps_format, *[int(i) for i in mspSensorGpsDataMessage.values()])
+            # Send GPS data
+            if not board.send_RAW_msg(MSPy.MSPCodes['MSP2_SENSOR_GPS'], data=gps_data):
+                print("MSP2_SENSOR_GPS not sent!")
+        print(time.monotonic()-prev)
 # profile.runcall(foo)
 profile.run('foo()')
 ps = pstats.Stats(profile)
---
+++ Examples/test_nav_althold_baro_rangefinder.py
@@ -0,0 +1,84 @@
+
+import time
+import struct
+
+from yamspy import MSPy, msp_ctrl
+
+# $ python -m yamspy.msp_proxy --ports 54310 54320 54330 54340
+serial_port = 54320
+# serial_port = '/dev/ttyACM0'
+FC_SEND_LOOP_TIME = 1/10
+
+
+msp2_range_format = '<Bi' # https://docs.python.org/3/library/struct.html#format-characters
+range_template = {
+             'quality': 0,      # uint8_t - [0;255] - INAV 5.1 ignores this value!
+             'distanceMm': 0,   # int32_t - Negative value for out of range
+}
+
+msp2_baro_format = '<BIfh' # https://docs.python.org/3/library/struct.html#format-characters
+baro_template = {
+             'instance': 0,      # uint8_t
+             'timeMs': 0,        # uint32_t - ignored by the FC
+             'pressurePa': 0.0,  # float
+             'temp': 0           # int16_t - centi-degrees C
+}
+
+with MSPy(device=serial_port, loglevel='WARNING', baudrate=115200, use_tcp=True) as board:
+    command_list = ['MSP_API_VERSION', 'MSP_FC_VARIANT', 'MSP_FC_VERSION', 'MSP_BUILD_INFO',
+                    'MSP_BOARD_INFO', 'MSP_UID', 'MSP_ACC_TRIM', 'MSP_NAME', 'MSP_STATUS',
+                    'MSP_STATUS_EX','MSP_BATTERY_CONFIG', 'MSP_BATTERY_STATE', 'MSP_BOXNAMES']
+    for msg in command_list:
+        if board.send_RAW_msg(MSPy.MSPCodes[msg], data=[]):
+            dataHandler = board.receive_msg()
+            board.process_recv_data(dataHandler)
+    try:
+        mspSensorRangefinderDataMessage = range_template.copy()
+        mspSensorRangefinderDataMessage['quality'] = 200
+        mspSensorRangefinderDataMessage['distanceMm'] = 50
+
+        # https://www.mide.com/air-pressure-at-altitude-calculator
+        # 101325.00 @ 25oC = 0m
+        # Allow the FC to calculate the zero????
+        mspSensorBaroDataMessage = baro_template.copy()
+        mspSensorBaroDataMessage['instance'] = 1
+        mspSensorBaroDataMessage['pressurePa'] = 101208.95 # 10m above sea level
+        mspSensorBaroDataMessage['temp'] = 25*100 # centi-degrees C
+        
+        baro_data = struct.pack(msp2_baro_format, *mspSensorBaroDataMessage.values())
+        range_data = struct.pack(msp2_range_format, *mspSensorRangefinderDataMessage.values())
+
+        # Send some messages to initialize / calibrate the barometer
+        for i in range(50):
+            print("Initial messages ", time.monotonic())
+
+            # Send Baro data
+            if board.send_RAW_msg(MSPy.MSPCodes['MSP2_SENSOR_BAROMETER'], data=baro_data):
+                print(f"MSP2_SENSOR_BAROMETER data {baro_data} sent!")
+
+            # # Send rangefinder data
+            # if board.send_RAW_msg(MSPy.MSPCodes['MSP2_SENSOR_RANGEFINDER'], data=range_data):
+            #     print(f"MSP2_SENSOR_RANGEFINDER data {range_data} sent!")
+
+            time.sleep(FC_SEND_LOOP_TIME)
+
+        while True:
+            # Ask altitude data (maybe I should ask for MSP_SONAR_ALTITUDE as well)
+            prev_time = time.monotonic()
+
+            # Send Baro data
+            if board.send_RAW_msg(MSPy.MSPCodes['MSP2_SENSOR_BAROMETER'], data=baro_data):
+                print(f"MSP2_SENSOR_BAROMETER data {baro_data} sent!")
+
+            # # Send rangefinder data
+            # if board.send_RAW_msg(MSPy.MSPCodes['MSP2_SENSOR_RANGEFINDER'], data=range_data):
+            #     print(f"MSP2_SENSOR_RANGEFINDER data {range_data} sent!")
+
+
+            time.sleep(FC_SEND_LOOP_TIME)
+
+    except KeyboardInterrupt:
+        print("stop")
+    finally:
+        pass
+        #board.reboot()
